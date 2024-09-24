a1dbe7f7b3c90bc2ddad03a2ce9df3ebd64b2c85
Escanole <147304808+Escanole@users.noreply.github.com>
2024-09-24 10:28:13 +0700

    Update admin.py

--- module_group/admin.py
+++ module_group/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ModuleGroup
+from .models import ModuleGroup, Module
 
 admin.site.register(ModuleGroup)
+admin.site.register(Module)
