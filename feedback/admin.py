31ff449f2c69a0e955fdb85f007681cbe3fd824f
Escanole <147304808+Escanole@users.noreply.github.com>
2024-09-24 09:49:39 +0700

    Delete feedback directory

--- feedback/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import InstructorFeedback, CourseFeedback, TrainingProgramFeedback
-
-admin.site.register(InstructorFeedback)
-admin.site.register(CourseFeedback)
-admin.site.register(TrainingProgramFeedback)
