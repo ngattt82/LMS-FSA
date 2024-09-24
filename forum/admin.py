4ac3750b127fb5b46ba352af33520c8ac8495506
Escanole <147304808+Escanole@users.noreply.github.com>
2024-09-24 09:49:25 +0700

    Delete forum directory

--- forum/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ForumQuestion, ForumComment, Reply
-
-# Register your models here.
-admin.site.register(ForumQuestion)
-admin.site.register(ForumComment)
-admin.site.register(Reply)
